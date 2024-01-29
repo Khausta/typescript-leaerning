e69558772fe1ba5dec314ddfbad1cabb96d465a2
Khausta <anna.a.khaustova@gmail.com>
2024-01-29 20:00:38 +0300

    Objects

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 "use strict";
-function getFullName(firstname, surname) {
-    return `${firstname} ${surname}`;
+function getFullName(userEntity) {
+    // userEntity.skills - не можем обратиться 
+    return `${userEntity.firstname} ${userEntity.surname}`;
 }
-const getFullNameArror = (firstname, surname) => {
-    return `${firstname} ${surname}`;
+const user = {
+    firstname: 'Anna',
+    surname: 'Khaustova',
+    city: 'Mos',
+    age: 30,
+    skills: {
+        dev: true,
+        design: true
+    }
 };
-// function getFullName(firstname, surname) {
-//     //такая явная проверка в ts может быть применена только в 2-х случаях:
-//     // 1. Когда данные приходят из внешней системы
-//     if (typeof firstname !== 'string') {
-//         throw new Error('!!!');
-//     }
-//     return `${firstname} ${surname}`;
-// }
-console.log(getFullName("Anna", "Khaustova"));
+console.log(getFullName(user));
--- app.ts
+++ app.ts
@@ -1,19 +1,17 @@
-function getFullName(firstname: string, surname: string): string {
-    return `${firstname} ${surname}`;
+function getFullName(userEntity: {firstname: string, surname: string}): string {
+    // userEntity.skills - не можем обратиться 
+    return `${userEntity.firstname} ${userEntity.surname}`;
 }
 
-const getFullNameArror = (firstname: string, surname: string): string => {
-    return `${firstname} ${surname}`;
+const user = {
+    firstname: 'Anna',
+    surname: 'Khaustova',
+    city: 'Mos',
+    age: 30,
+    skills: {
+        dev: true,
+        design: true
+    }
 }
 
-// function getFullName(firstname, surname) {
-//     //такая явная проверка в ts может быть применена только в 2-х случаях:
-//     // 1. Когда данные приходят из внешней системы
-    
-//     if (typeof firstname !== 'string') {
-//         throw new Error('!!!');
-//     }
-//     return `${firstname} ${surname}`;
-// }
-
-console.log(getFullName("Anna", "Khaustova"));
+console.log(getFullName(user));
