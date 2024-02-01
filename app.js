08e31782f2f90a3add6f2c1ba3deb7de84784ba3
Khausta <anna.a.khaustova@gmail.com>
2024-02-01 19:42:18 +0300

    TYPE ALIAS

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
 "use strict";
 //Literal Types
-// example with fetch request
-// function fetchWithAuth(url: string, method: string) { //так в method можем что угодно передать, даже не post и get, а что угодно
-//     //пробудем enam
-// }
-//пример с enum
-var FetchMethods;
-(function (FetchMethods) {
-    FetchMethods["GET"] = "GET";
-    FetchMethods["POST"] = "POST";
-})(FetchMethods || (FetchMethods = {}));
 function fetchWithAuth(url, method) {
     // return fetch(url, method); //ts(2559) - why?
-}
-// fetchWithAuth('http://dsfgjfd.ro', FetchMethods.POST).catch(err => console.error(err.message));
-//ПРИМЕНИМ LITERAL TYPES
-// function fetchWithAuthL(url: string, method: "get" | "post") {
-//     if (method === "get") {
-//         // fetch(url, method);// ошибка тоже
-//     } 
-// }
-function fetchWithAuth2(url, method) {
-    // return fetch(url, method); //ts(2559) - why? 
-}
-fetchWithAuth2("sdg", "get");
-function fetchWithAuth3(url, method) {
-    // return fetch(url, method); //ts(2559) - why? 
-    // return 2; //не проходит
     return 1;
 }
-const a = 1;
-fetchWithAuth3('wer', 2);
-fetchWithAuth3('wer', a); //каст к типу
--- app.ts
+++ app.ts
@@ -1,43 +1,53 @@
-//Literal Types
+//Type Alias
  
+type httpMethod = 'post' | 'get'; //union
+type coolString = string; //так определяется одельный тип
 
-// example with fetch request
-// function fetchWithAuth(url: string, method: string) { //так в method можем что угодно передать, даже не post и get, а что угодно
-//     //пробудем enam
-// }
-
-//пример с enum
-enum FetchMethods {
-    GET = 'GET',
-    POST = 'POST'
-}
-function fetchWithAuth(url: string, method: FetchMethods) {
+function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
     // return fetch(url, method); //ts(2559) - why?
-   
-}
-// fetchWithAuth('http://dsfgjfd.ro', FetchMethods.POST).catch(err => console.error(err.message));
-
-//ПРИМЕНИМ LITERAL TYPES
-// function fetchWithAuthL(url: string, method: "get" | "post") {
-//     if (method === "get") {
-//         // fetch(url, method);// ошибка тоже
-//     } 
-// }
-
-function fetchWithAuth2(url: string, method: "get" | "post") {
-    // return fetch(url, method); //ts(2559) - why? 
-}
-fetchWithAuth2("sdg", "get");
-
-
-
-
-function fetchWithAuth3(url: string, method: 1 | 2): 1 | -1 {
-    // return fetch(url, method); //ts(2559) - why? 
-    // return 2; //не проходит
     return 1;
 }
 
-const a = 1;
-fetchWithAuth3('wer', 2);
-fetchWithAuth3('wer', a as 2); //каст к типу
+
+type User = {
+    //эту типизацию можем вынести в Type Alias
+    name: string;
+    age: number;
+    skills: string[]
+}
+
+// let user: {
+//     //эту типизацию можем вынести в Type Alias
+//     name: string;
+//     age: number;
+//     skills: string[]
+// } = {
+//     name: 'sada',
+//     age: 22,
+//     skills: ['1', '2']
+// }
+let user: User = {
+    name: 'sada',
+    age: 22,
+    skills: ['1', '2']
+}
+
+//Intersection
+//создаем еще один Type Alias
+type Role = {
+    id: number,
+    name: string //пересекающаяся чать - ОБЩИЕ ПОЛЯ(если сравнить с User)
+}
+
+// type UserWithRole = User & Role; //лучше объединять отжельно
+type UserWithRole = {
+    user: User,
+    role: Role
+}
+//совмещены два алиаса и требуются все ключи указанные в обоих алиасах
+let user2: UserWithRole = {
+    name: 'sada', //Object literal may only specify known properties, and 'name' does not exist in type 'UserWithRole'.ts(2353)
+    age: 22,
+    skills: ['1', '2'],
+    id: 1
+}
