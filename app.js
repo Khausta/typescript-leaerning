3c2372cf2d56075fd977b207ec33c5f9c9f3fa50
Khausta <anna.a.khaustova@gmail.com>
2024-02-01 23:03:16 +0300

    OPTIONAL

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 "use strict";
-// Interfaces VS Types
-// это может вводить в заблуждении при codereview!
-// удобно, если использовать стороннюю библиотеку и 
-//нет возможности заэкстендить како-то интерфейс
-const user = {
-    name: 'kljfd',
-    id: 2
+// Optional
+const user1 = {
+    name: 'sdfd',
+    password: 'dfgkj@'
 };
+//тоже валидно
+const user2 = {
+    name: 'sdfd'
+};
+const user3 = {
+    role: 'Admin'
+};
+const lenovo2134 = {
+    origin: 'Japan',
+    //нужно явно указать undefined если нет discount
+    discount: 25
+};
+//здесь  second?: number ЭКВИВАЛЕНТНО second: number | undefined
+function multiply(first, second) {
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+//можем указать дефолтное значение, если нам не передали значение second
+function multiply2(first, second = 5) {
+    return first * second;
+}
+multiply(5);
+multiply(2, 2);
+function testPass(user) {
+    var _a;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+    // эквивалент
+    const t2 = user.password ? user.password.type : undefined;
+}
+// ?? - проверка значения перед ?? на null or undefined
+function test(param) {
+    const t = param !== null && param !== void 0 ? param : multiply(2);
+}
+function testUser(user) {
+    const t = user.password.type;
+}
--- app.ts
+++ app.ts
@@ -1,36 +1,87 @@
-// Interfaces VS Types
+// Optional
 
-// 1 отличие
+//если какое-то свойство мы можем не передать используй "?" после ключа 
 interface User {
+    name: string,
+    password?: string
+}
+
+const user1: User = {
+    name: 'sdfd',
+    password: 'dfgkj@'
+}
+//тоже валидно
+const user2: User = {
+    name: 'sdfd'
+}
+
+type Role = {
+    role: string,
+    roleId?: number
+}
+
+const user3: Role = {
+    role: 'Admin'
+}
+
+// что с Undefined
+interface Product {
+    origin: string,
+    discount: number | undefined  //нужно будет обязательно это свойто указывать в объектах
+}
+
+const lenovo2134: Product = {
+    origin: 'Japan',
+    //нужно явно указать undefined если нет discount
+    discount: 25
+}
+
+
+//здесь  second?: number ЭКВИВАЛЕНТНО second: number | undefined
+function multiply(first: number, second?: number): number {
+    if(!second) {
+        return first * first;
+    }
+    return first * second;
+}
+
+//можем указать дефолтное значение, если нам не передали значение second
+function multiply2(first: number, second: number = 5): number {
+    return first * second;
+}
+
+multiply(5);
+multiply(2, 2);
+
+
+
+
+interface UserPro {
     name: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    };
 }
 
-interface User {
-    id: number
-} // перезапись
-
-// это может вводить в заблуждении при codereview!
-// удобно, если использовать стороннюю библиотеку и 
-//нет возможности заэкстендить како-то интерфейс
-const user: User = {
-    name: 'kljfd',
-    id: 2
+function testPass(user: UserPro) {
+    const t = user.password?.type;
+    // эквивалент
+    const t2 = user.password ? user.password.type : undefined;
 }
 
-//будет ошибка. Types не перезаписываются
-type User1 = {
-    name: string
+// ?? - проверка значения перед ?? на null or undefined
+function test(param?: string) {
+    const t = param ?? multiply(2);
 }
 
-// type User1 = {
-//     id: number
-// }               //Duplicate identifier 'User1'.ts(2300)
-
-// в type можем использовать union
-type ID = string | number; 
-
-interface IDI {
-    ID: string | number
+// ! - кога уверены что точно будет свойсвто
+interface User0 {
+    name: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    };
 }
 
-
+function testUser(user: User0) {
+    const t = user.password!.type;
+}
