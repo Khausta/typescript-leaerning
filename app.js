d822e95407eb1c0c5361f73c0a496dec513b41a7
Khausta <anna.a.khaustova@gmail.com>
2024-02-05 18:55:07 +0300

    Asserts

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
 "use strict";
-const user = {
-    name: 'Vasya',
-    email: 'vasya@mail.ru',
-    login: 'vasya-vasya'
-};
-//обрати внимание на тип который выводится после проверок
-function getId(id) {
-    // if (typeof id === 'string') { //-- можем заменить type guard
-    if (isString(id)) {
-        console.log(id);
+//допустим с сервера приходит объект и мы не хнаем что там будет
+const a = {};
+assertUser(a);
+a.name = 'Vasya';
+function assertUser(obj) {
+    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+        return;
     }
-    else {
-        console.log(id);
-    }
-}
-// type guard  - это f-я
-function isString(x) {
-    return typeof x === 'string';
-}
-//type guard
-function isAdmin(user) {
-    return 'role' in user;
-}
-function sertRoleZero(user) {
-    if (isAdmin(user)) {
-        user.role = 0;
-    }
-    else {
-        throw new Error('пользователь не является админом');
-    }
-}
-function isAdminAlternative(user) {
-    return user.role !== undefined;
+    throw new Error('Не польхователь');
 }
--- app.ts
+++ app.ts
@@ -1,58 +1,20 @@
 // Type Guard
 interface User {
     name: string;
-    email: string;
-    login: string;
 }
 
-const user: User = {
-    name: 'Vasya',
-    email: 'vasya@mail.ru',
-    login: 'vasya-vasya'
-} 
+//допустим с сервера приходит объект и мы не хнаем что там будет
+const a = {};
 
-interface Admin {
-    name: string;
-    role: number
+assertUser(a);
+a.name = 'Vasya';
+
+function assertUser(obj: unknown): asserts obj is User {
+    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+        return;
+    } 
+    throw new Error('Не польхователь');
 }
-
-
-//обрати внимание на тип который выводится после проверок
-function getId(id: number | string) {
-    // if (typeof id === 'string') { //-- можем заменить type guard
-    if (isString(id)) {
-        console.log(id)
-    } else {
-        console.log(id);
-    }
-    
- }
-
-
- // type guard  - это f-я
- function isString(x: string | number): x is string { //вернут true или false
-    return typeof x === 'string';
- }
-
- //type guard
- function isAdmin(user: User | Admin): user is Admin {
-    return 'role' in user;
- }
-
- function sertRoleZero(user: User | Admin) {
-    if (isAdmin(user)) {
-        user.role = 0;
-    } else {
-        throw new Error('пользователь не является админом');
-    }
- }
-
- function isAdminAlternative(user: User | Admin): user is Admin {
-    return (user as Admin).role !== undefined;
- }
-
-
-
 
 
 
