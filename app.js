ef48b4148447f138788223d06baa4a760c2ee15f
Khausta <anna.a.khaustova@gmail.com>
2024-02-05 16:56:54 +0300

    NULL

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
 "use strict";
-// Never
-//то есть никаогда ничего не возвращает
-// 1 case
-function generateError(message) {
-    throw new Error(message);
-}
-//2 case 
-function dumpError() {
-    while (true) { }
-}
-//3 case - с рекурсией
-function rec() {
-    return rec();
-}
-// const a: never; //- не валидно
-// const a: never = 2; //-не валидно
-// const a: never = null; //-не валидно
-const a = undefined; //валидно
-function processAction(action) {
-    switch (action) {
-        case 'refund':
-            //...do something
-            break;
-        case 'checkout':
-            //...do something
-            break;
-        case 'rejected':
-            //...do something
-            console.log('rejected');
-            break;
-        default:
-            //чтобы обработать ошибку правильно
-            // на этапе Compiler Time обрабатываем
-            const _ = action; // _ означает переменную которую никогда не будем испольховать
-            throw new Error('Нет такого action');
+const n = null; //только null приримает
+const n1 = null;
+const n3 = null;
+const n4 = null;
+const n5 = null;
+function getUser() {
+    if (Math.random() > 0.5) {
+        return null;
+    }
+    else {
+        return {
+            name: 'Vasya'
+        };
     }
 }
-processAction('rejected');
-// processAction('gfd'); --- не скомпилируется файл
-// case 5 - Исчерпывающая проверка - Exhaustive check
-function isString(x) {
-    if (typeof x === 'string') {
-        return true;
-        // } else {
-        //     return false
-        // } может юыть по другому
-    }
-    else if (typeof x === 'number') {
-        return false;
-    } // получается что остается третий вариант, который мы должны обработать
-    //например добавить функцию типа never
-    generateError('err oops');
+const user = getUser();
+if (user) {
+    const userName = user.name;
 }
--- app.ts
+++ app.ts
@@ -1,60 +1,28 @@
-// Never
-//то есть никаогда ничего не возвращает
-// 1 case
-function generateError(message: string): never {
-    throw new Error(message);
-}
-//2 case 
-function dumpError(): never {
-    while (true) {}
-}
-//3 case - с рекурсией
-function rec(): never {
-    return rec();
+const n: null = null; //только null приримает
+const n1: any = null;
+const n3: string = null; 
+const n4: boolean = null; 
+const n5: undefined = null; 
+const n6: unknown = null; 
+
+interface User {
+    name: string;
 }
 
-// const a: never; //- не валидно
-// const a: never = 2; //-не валидно
-// const a: never = null; //-не валидно
-const a: void = undefined; //валидно
-
-// 4 case
-type paymentAction = 'refund' | 'checkout' | 'rejected';
-function processAction(action: paymentAction) {
-    switch (action) {
-        case 'refund':
-            //...do something
-            break;
-        case 'checkout':
-            //...do something
-            break;
-        case 'rejected':
-            //...do something
-            console.log('rejected');
-            break;
-        default:
-            //чтобы обработать ошибку правильно
-            // на этапе Compiler Time обрабатываем
-            const _: never = action; // _ означает переменную которую никогда не будем испольховать
-            throw new Error('Нет такого action');
-  
+function getUser() {
+    if (Math.random() > 0.5) {
+        return null;
+    } else {
+        return {
+            name: 'Vasya'
+        } as User
     }
 }
 
-processAction('rejected');
-// processAction('gfd'); --- не скомпилируется файл
-
-// case 5 - Исчерпывающая проверка - Exhaustive check
-function isString(x: number | string): boolean {
-    if (typeof x === 'string') {
-        return true;
-    // } else {
-    //     return false
-    // } может юыть по другому
-    } else if (typeof x === 'number') {
-        return false;
-    } // получается что остается третий вариант, который мы должны обработать
-    //например добавить функцию типа never
-    generateError('err oops');
+const user = getUser();
+if (user) {
+    const userName = user.name;
 }
+
+
 
