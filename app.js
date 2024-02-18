ce0d8f1d4d8cd7f171241cad35546dfe8b9d7a8e
Khausta <anna.a.khaustova@gmail.com>
2024-02-18 15:01:33 +0300

    Static

--- app.js
+++ app.js
@@ -1,99 +1,25 @@
 "use strict";
-// Необходимо сделать корзину (Cart) на сайте,
-// которая имееет список продуктов (Product), добавленных в корзину
-// и переметры доставки (Delivery). Для Cart реализовать методы:
-// Добавить продукт в корзину
-// Удалить продукт из корзины по ID
-// Посчитать стоимость товаров в корзине
-// Задать доставку
-// Checkout - вернуть что всё ок, если есть продукты и параметры доставки
-// Product: id, название и цена
-// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
-class Product {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+// Static properties
+class UserService {
+    //может быть использовано async await
+    // например static async getUserById(id: number) { ...await new Promise() }
+    static getUserById(id) {
+        // return this.db.findById(id);
+        return UserService.db.findById(id);
+    }
+    create() {
+        UserService.db;
     }
 }
-class Delivery {
-    constructor(date) {
-        this.date = date;
-    }
-}
-class HomeDelivery extends Delivery {
-    constructor(date, address) {
-        super(date);
-        this.address = address;
-    }
-}
-class ShopDelivery extends Delivery {
-    constructor(date, marketId) {
-        super(new Date());
-        this.marketId = marketId;
-    }
-}
-class Cart {
-    constructor() {
-        this.products = [];
-    }
-    addProduct(product) {
-        this.products.push(product);
-    }
-    deleteProduct(productId) {
-        this.products = this.products.filter((product) => product.id !== productId);
-    }
-    getSum() {
-        if (this.products.length) {
-            return this.products
-                .map((p) => p.price)
-                .reduce((currentSum, currentPrice) => currentSum + currentPrice);
-        }
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkout() {
-        if (this.products.length === 0) {
-            throw new Error("Нет товара в корзине");
-        }
-        if (!this.delivery) {
-            throw new Error("Не указан способ доставки");
-        }
-        return { success: true };
-    }
-}
-const cart = new Cart();
-cart.addProduct(new Product(1, 'Apple', 10));
-cart.addProduct(new Product(2, 'Chocolad', 20));
-cart.addProduct(new Product(3, 'Cake', 30));
-cart.deleteProduct(1);
-cart.deleteProduct(2);
-cart.deleteProduct(3);
-cart.setDelivery(new HomeDelivery(new Date(), 'Lobnya'));
-console.log(cart.getSum());
-console.log(cart.checkout());
-// class Cart {
-//   _products: Product[] = [];
-//   get products() {
-//     return this._products;
-//   }
-//   addProduct(name: string, price: number, id: number) { 
-//     this._products.push(new Product(name, price, id));
-//   }
-//   deleteProduct(name: string) {
-//     this._products = this._products.filter(p => p.name !== name);
-//   }
-//   get getTotalPrice() {
-//     let sum: number = 0;
-//     this._products.map(el => sum += el.price)
-//     return sum;
-//   }
-// }
-// const cart = new Cart();
-// cart.addProduct('Apple', 20, 1);
-// cart.addProduct('Orange', 21, 2);
-// cart.addProduct('Bread', 10, 3);
-// // cart.deleteProduct('Apple');
-// console.log(cart.getTotalPrice);
-// console.log(cart.products);
+//пример со статичным блоком кода
+(() => {
+    UserService.db = 'fd';
+    //здесь при инициализации сразу происходит присвоение.
+    // async await не можем испольховать
+})();
+UserService.db; //статические свойства можно использовать без инстанциирования и мы можем обращаться как к объекту
+UserService.getUserById(1); //к статичному методу класса можно обращаться как к методу объекта
+// UserService.create(); --- Ошибка, не можем получить доступ к нестатичному свойству без истанциирования
+// НО! Можем получить доступ к статичному методу через нестатичный метод 
+const inst = new UserService();
+inst.create();
--- app.ts
+++ app.ts
@@ -1,94 +1,38 @@
+// Static properties
+class UserService {
+  static db: any; //может быть private or protected
 
-// Необходимо сделать корзину (Cart) на сайте,
+  //может быть использовано async await
+  // например static async getUserById(id: number) { ...await new Promise() }
+  static getUserById(id: number) {
+    // return this.db.findById(id);
+    return UserService.db.findById(id); 
 
-// которая имееет список продуктов (Product), добавленных в корзину
+  }
 
-// и переметры доставки (Delivery). Для Cart реализовать методы:
+  create() {
+    UserService.db;
+  }
 
-// Добавить продукт в корзину
-// Удалить продукт из корзины по ID
-// Посчитать стоимость товаров в корзине
-// Задать доставку
-// Checkout - вернуть что всё ок, если есть продукты и параметры доставки
-// Product: id, название и цена
-
-// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
-
-
-class Product {
-  constructor(
-    public id: number,
-    public title: string,
-    public price: number,
-  ) {}
-}
-
-class Delivery {
-  constructor(
-    public date: Date
-  ) {}
-}
-
-class HomeDelivery extends Delivery {
-  constructor( date: Date, public address: string) {
-    super(date);
+  //пример со статичным блоком кода
+  static {
+    UserService.db = 'fd';
+    //здесь при инициализации сразу происходит присвоение.
+    // async await не можем испольховать
   }
 }
 
-class ShopDelivery extends Delivery {
-  constructor( date: Date, public marketId: number) {
-    super(new Date());
+UserService.db //статические свойства можно использовать без инстанциирования и мы можем обращаться как к объекту
+UserService.getUserById(1); //к статичному методу класса можно обращаться как к методу объекта
+// UserService.create(); --- Ошибка, не можем получить доступ к нестатичному свойству без истанциирования
+// НО! Можем получить доступ к статичному методу через нестатичный метод 
+const inst = new UserService();
+inst.create();
 
-  }
-}
 
-type DeliveryOptions = HomeDelivery | ShopDelivery;
 
-class Cart {
-  private products: Product[] = [];
-  private delivery: DeliveryOptions;
 
-  public addProduct(product: Product): void {
-    this.products.push(product);
-  }
 
-  public deleteProduct(productId: number): void {
-    this.products = this.products.filter((product: Product) => product.id !== productId);
-  }
-
-  public getSum(): number | void {
-    if(this.products.length) {
-      return this.products
-        .map((p: Product) => p.price)
-        .reduce((currentSum: number, currentPrice: number): number => currentSum + currentPrice)
-    }
-  }
-
-  public setDelivery(delivery: DeliveryOptions): void {
-    this.delivery = delivery;
-  }
-
-  public checkout() {
-    if(this.products.length === 0) {
-      throw new Error("Нет товара в корзине")
-    } 
-    if (!this.delivery) {
-      throw new Error("Не указан способ доставки")
-    }
-    return {success: true};
-  }
-}
-
-const cart = new Cart();
-cart.addProduct(new Product(1, 'Apple', 10));
-cart.addProduct(new Product(2, 'Chocolad', 20));
-cart.addProduct(new Product(3, 'Cake', 30));
-cart.deleteProduct(1);
-cart.deleteProduct(2);
-cart.deleteProduct(3);
-cart.setDelivery(new HomeDelivery(new Date(), 'Lobnya'));
-console.log(cart.getSum());
-console.log(cart.checkout());
 
 
 
