b400c0852fb1385ce508adb631ef83a1ac6e220e
Khausta <anna.a.khaustova@gmail.com>
2024-02-20 18:28:13 +0300

    Including and excluding files

--- app.ts
+++ app.ts
@@ -1,18 +1,10 @@
-// Необходимо реализовать абстрактный класс Logger с 2-мя 
-// методами абстрактным - log(message): void и printDate - 
-// выводящий в log дату.
-// К нему необходимо сделать реальный класс, 
-// который бы имел метод: logWithDate,выводящий 
-// сначала дату, а потом заданное сообщение
-
+//
 abstract class Logger {
   abstract log(message: string): void;
 
   printDate(date: Date): void {
    this.log(date.toString());
   }
-
- 
 }
 
 class RealLogger extends Logger {
@@ -28,4 +20,5 @@ class RealLogger extends Logger {
 
 const log = new RealLogger();
 log.logWithDate("message!");
-  
+
+
